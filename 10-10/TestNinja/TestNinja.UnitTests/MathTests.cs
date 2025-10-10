c6c7fc0f9ca0b3dee1df78bf6988acc3e5dd94b2
oliwia chojnacka <ochojnacka@protonmail.com>
2025-10-10 17:07:16 +0200

    math tests

---
+++ 10-10/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestNinja.Fundamentals;
+using Math = TestNinja.Fundamentals.Math;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var math = new Math();
+            var result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
