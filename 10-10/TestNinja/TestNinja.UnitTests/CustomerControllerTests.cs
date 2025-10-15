dd6562e33b36176ab8099ce7090f7150636ee639
oliwia chojnacka <ochojnacka@protonmail.com>
2025-10-15 16:02:44 +0200

    TypeOf tests

---
+++ 10-10/TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOK()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
