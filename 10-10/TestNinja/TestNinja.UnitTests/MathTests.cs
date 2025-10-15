fabfdae5e9b9bd23c6e0b9a0573a9b2bd1625c9f
oliwia chojnacka <ochojnacka@protonmail.com>
2025-10-15 15:19:34 +0200

    SetUp function

--- 10-10/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ 10-10/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -10,12 +10,44 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        //  SetUp
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void TestMethod1()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            //var math = new Math();
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            //var math = new Math();
+            var result = _math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            //var math = new Math();
+            var result = _math.Max(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnSameArgument()
+        { 
+            //var math = new Math();
+            var result = _math.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
         }
     }
 }
