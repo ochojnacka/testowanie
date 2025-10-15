5416186a004703fc6142a6d1ce87daf4bc2819e5
oliwia chojnacka <ochojnacka@protonmail.com>
2025-10-15 15:37:29 +0200

    test on strings

---
+++ 10-10/TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void Format_WhenCalled_ReturnsFormattedString()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+
+            // Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            // General
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("abc"));
+            Assert.That(result, Does.Match("<strong>.*</strong>"));
+
+        }
+    }
+}
