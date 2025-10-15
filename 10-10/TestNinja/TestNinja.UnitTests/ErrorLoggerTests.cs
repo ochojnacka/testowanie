14622dd7864275245c61d476e2837873d1e9f4e5
oliwia chojnacka <ochojnacka@protonmail.com>
2025-10-15 16:17:45 +0200

    ErrorLogger void function cases

---
+++ 10-10/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("error");
+            Assert.That(logger.LastError, Is.EqualTo("error"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_EmptyOrWhitespaceError_ThrowsArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+    }
+}
